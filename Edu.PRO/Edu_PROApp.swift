c28b8cb841705c6a5862f981877956d993bb8130
João Victor de Souza Guedes <espro.joao@WMM01693.local>
2023-10-03 14:41:43 -0300

    Initial commit

---
+++ Edu.PRO/Edu_PROApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edu_PROApp.swift
+//  Edu.PRO
+//
+//  Created by João Victor de Souza Guedes on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Edu_PROApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
